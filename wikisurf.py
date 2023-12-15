1bb87c88d6883436d13dc2bed44e0b73fb7bdc9f
Matthew McLaren <mmclaren@purdue.edu>
2023-12-15 14:00:53 -0500

    Added logging, organized imports,

--- wikisurf.py
+++ wikisurf.py
@@ -1,13 +1,14 @@
+import logging
+import os
 import requests
 import sys
 import time
-import os
-from termcolor import colored
+
 from bs4 import BeautifulSoup
+from termcolor import colored
 
 from rw_table import write_ref_file
 
-init_url = '/wiki/Main_Page'
 host_url = 'https://en.wikipedia.org'
 output_folder = 'out'
 
@@ -20,22 +21,18 @@ slug_permalink :
 
 def find_ref_in_url(url : str) -> list[str]:
     # send a request to get the packet and open it for html parsing
-    try :
-        resp = requests.get(url)
-    except Exception as err :
-        print(colored(f'request unable to be made : Error {err} occured', 'yellow'))
-        return None
+    resp = requests.get(url)
     soup = None
     if resp.status_code == 200 :
         soup = BeautifulSoup(resp.content, 'html.parser')
     elif resp.status_code == 429 :
         # handle instance of too many requests sent to avoid timeout
-        print(colored(f'[Error 429 occured : pausing for {int(resp.headers['Retry-After'])}]', 'yellow'))
+        logging.info(colored(f'[Error 429 occured : pausing for {int(resp.headers['Retry-After'])}]', 'yellow'))
         time.sleep(int(resp.headers['Retry-After']))
-        print(colored('Resuming...', 'yellow'))
+        logging.info(colored('Resuming...', 'yellow'))
         return find_ref_in_url(url)
     else :
-        print(colored(f'Error {resp.status_code} occured : unsupported handle', 'red'))
+        logging.info(colored(f'Error {resp.status_code} occured : unsupported handle [skipping]', 'red'))
         return None
 
     # go into the html file and find only the parser output associated with the content body
@@ -53,20 +50,24 @@ def find_ref_in_url(url : str) -> list[str]:
     return ref_list_for_url
 
 if __name__ == "__main__":
+    # config logger
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
     # make sure that there is an input string
     if len(sys.argv) != 3:
-        print(usage_txt)
+        logging.info(usage_txt)
         sys.exit(-1)
     else :
         if str(sys.argv[1]).find(host_url) != -1 :
-            print(colored("slug_permalink invalid", 'red'), end='')
-            print(usage_txt)
+            logging.info(colored("slug_permalink invalid", 'red'), end='')
+            logging.info(usage_txt)
             sys.exit(-1)
         try:
             int(sys.argv[2])
         except:
-            print(colored("max_depth invalid", 'red'), end='')
-            print(usage_txt)
+            logging.info(colored("max_depth invalid", 'red'), end='')
+            logging.info(usage_txt)
             sys.exit(-1)
 
     # clear the output folder
@@ -84,7 +85,7 @@ if __name__ == "__main__":
     num_files = 0
     while cur_depth != max_depth :
         # tell console progress at depth
-        print(f'beginning at depth {cur_depth}')
+        logging.info(f'beginning at depth {cur_depth}')
 
         # make a new queue and go through until old queue is empty
         new_queue = list()
@@ -96,8 +97,9 @@ if __name__ == "__main__":
             # retrieve list of references to that url
             list_of_refs = find_ref_in_url(host_url + slug_permalink)
             if list_of_refs == None :
-                print(colored(f'{host_url + slug_permalink} not reachable (skipping)', 'yellow'))
                 continue
+            list_of_refs = list(set(list_of_refs))
+            list_of_refs.sort(reverse=False)
 
             # write to a file the output
             write_count = write_ref_file(output_folder + '\\' + slug_permalink.replace('/', '(~)') + '.txt', list_of_refs)
@@ -111,13 +113,13 @@ if __name__ == "__main__":
                     new_queue.append(ref)
 
             # tell console progress of queue
-            print(f'{(((i + 1) / max_queue_len) * 100):5.4}%')
+            logging.info(f'{(((i + 1) / max_queue_len) * 100):5.4}%')
         queue = new_queue
         cur_depth += 1
 
-    # print total number of links wrote to the folder
-    print(colored(f'Total number of writes is {num_writes}', 'cyan'))
-    print(colored(f'Total number of files in folder is {num_files}', 'cyan'))
+    # log total number of links wrote to the folder
+    logging.info(colored(f'Total number of writes is {num_writes}', 'cyan'))
+    logging.info(colored(f'Total number of files in folder is {num_files}', 'cyan'))
 
     # end program
     sys.exit(0)
